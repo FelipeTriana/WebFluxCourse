71e311ce07e5b6663f7cd028aba9d615d05490e3
Felipe Triana <felipetrianago@gmail.com>
2024-10-14 16:17:33 -0500

    Entendiendo el api reactiva

--- src/main/java/com/curso/webflux/models/dao/ProductoDao.java
+++ src/main/java/com/curso/webflux/models/dao/ProductoDao.java
@@ -3,6 +3,7 @@ package com.curso.webflux.models.dao;
 import com.curso.webflux.models.documents.Producto;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
+//Con mongodb al igual que con cualquier otra base de datos se puede trabajar con Spring Data JPA y sus herramientas de CrudRepository
 public interface ProductoDao extends ReactiveMongoRepository<Producto, String> {
 
 }
